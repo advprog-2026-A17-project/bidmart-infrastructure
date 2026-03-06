b9862fb1fba1dbcb17b0354496187f019734b634
aldofahrezy <aldofhrzy@gmail.com>
2026-03-06 14:58:05 +0700

    first commit

---
+++ src/main/java/id/ac/ui/cs/advprog/bidmartgateway/BidmartInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bidmartgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidmartInfrastructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BidmartInfrastructureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/bidmartgateway/BidmartInfrastructureApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.bidmartgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidmartInfrastructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
