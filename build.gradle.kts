b6acd3593db3c500e164300892162eedd50e0a83
aldofahrezy <aldofhrzy@gmail.com>
2026-05-18 12:54:48 +0700

    feat: add prometheus and grafana monitoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,6 +21,8 @@ extra["springCloudVersion"] = "2023.0.1"
 
 dependencies {
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
     implementation("io.jsonwebtoken:jjwt-api:0.12.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
